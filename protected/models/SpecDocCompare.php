f0dd62e1124bf896d25b563b49063097c03c8d74
boybe7 <boybe7oo@gmail.com>
2019-12-13 15:58:15 +0700

    add model

---
+++ protected/models/SpecDocCompare.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * This is the model class for table "spec_doc_compare".
+ *
+ * The followings are the available columns in table 'spec_doc_compare':
+ * @property integer $id
+ * @property integer $spec_id
+ * @property string $brand
+ * @property string $model
+ * @property string $price
+ * @property string $date_price
+ * @property string $attach_file
+ */
+class SpecDocCompare extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'spec_doc_compare';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('spec_id, brand, model, price, date_price, attach_file', 'required'),
+			array('spec_id', 'numerical', 'integerOnly'=>true),
+			array('brand', 'length', 'max'=>500),
+			array('model, attach_file', 'length', 'max'=>250),
+			array('price', 'length', 'max'=>10),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, spec_id, brand, model, price, date_price, attach_file', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'spec_id' => 'Spec',
+			'brand' => 'ยี่ห้อ',
+			'model' => 'รุ่น',
+			'price' => 'ราคา',
+			'date_price' => 'Date Price',
+			'attach_file' => 'Attach File',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('spec_id',$this->spec_id);
+		$criteria->compare('brand',$this->brand,true);
+		$criteria->compare('model',$this->model,true);
+		$criteria->compare('price',$this->price,true);
+		$criteria->compare('date_price',$this->date_price,true);
+		$criteria->compare('attach_file',$this->attach_file,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return SpecDocCompare the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
